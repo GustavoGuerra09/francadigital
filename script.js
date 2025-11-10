86af2bcd747fd7dbe83d5696cd012519206e565e
GustavoGuerra09 <gustavoguerra.dev@gmail.com>
2025-11-10 16:33:09 -0300

    javascript

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const campoNome = document.getElementById("campo-nome");
 campoNome.addEventListener("input",(e) => {
     console.log(e.target.value)
 })
+const caixa = document.getElementById("caixa");
+// caixa.innerHTML = "<h2> texto adicionado pelo javascript </h2>"
+const botao = document.getElementById("btn");
+const mensagem = document.getElementById("mensagem");
+botao.addEventListener("click", (e) => {
+    const valor = campoNome.value;
+    // mensagem.textContent = `ola ${valor}`; concatenaçao
+    mensagem.textContent = "Ola " + valor;
+})
