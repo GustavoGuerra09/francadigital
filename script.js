647e14b5a4ce66b03430e5526287e1ec0544e75b
GustavoGuerra09 <gustavoguerra.dev@gmail.com>
2025-11-10 16:06:31 -0300

    input

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 const titulo = document.getElementById('texto').innerText;
+const campoNome = document.getElementById("campo-nome");
+// (Variavel) => {} -> funçao anonima
+campoNome.getEventListener("input",(e) => {
+    console.log(e.target.value)
+})
