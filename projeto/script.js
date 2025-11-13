0c37f8ff24f3352d9ada6943eb75e74bc8e11b2d
GustavoGuerra09 <gustavoguerra.dev@gmail.com>
2025-11-13 01:38:27 -0300

    jv imagm

--- projeto/script.js
+++ projeto/script.js
@@ -7,5 +7,10 @@ bntAplicarCor.addEventListener("click", (e) => {
 });
 const chips = document.querySelectorAll(".chip");
 const galeria = document.getElementById("galeriaHero");
-const imagemInicial = chips[0].dataset.imagem;
-galeria.style.backgroundImage =`url()`
+const imagemInicial = chips[0].dataset.image;
+galeria.style.backgroundImage =`url(${imagemInicial})`;
+    chip.addEventListener("click", (e) => {
+        const novaImagem = e.target.dataset.image;
+    galeria.style.backgroundImage = `url(${novaImagem})`;
+});
+
