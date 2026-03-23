6088497f5a2cb927f7f66f19fc25f038d5b16cd3
GustavoGuerra09 <gustavoguerra.dev@gmail.com>
2026-03-22 21:13:04 -0300

    documentaçao

--- projeto/script.js
+++ projeto/script.js
@@ -1,16 +1,20 @@
+//funcionalidade que permite ao usuário mudar a cor de fundo do site.
 const seletorCor = document.getElementById("seletorCor"); 
 const bntAplicarCor = document.getElementById("btnAplicarCor");
 
 bntAplicarCor.addEventListener("click", (e) => {
     const cor = seletorCor.value;
+//document.documentElement.style.setProperty para alterar a variável --cor-fundo em tempo real.”
     document.documentElement.style.setProperty("--cor-fundo", cor);
 });
+//Quando o usuário clica em um dos elementos (chips), a imagem principal do site muda.”
 const chips = document.querySelectorAll(".chip");
 const galeria = document.getElementById("galeriaHero");
 const imagemInicial = chips[0].dataset.imagem;
 galeria.style.backgroundImage =`url(${imagemInicial})`
 //forEach : para cada objeto(repetir)
 chips.forEach((chip) =>{
+    //click para fazer o site reagir às ações do usuário.”
     chip.addEventListener("click", () => {
         galeria.classList.add("ativa");
         galeria.style.backgroundImage = `url(${chip.dataset.image})`
